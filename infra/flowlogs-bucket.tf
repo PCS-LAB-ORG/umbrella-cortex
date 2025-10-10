a0be073d9d1c62f71e0945120ca346ffb7a918b2
mdalbes <119487388+mdalbes@users.noreply.github.com>
2025-10-10 09:59:01 +0200

    acl bucket deprecated

--- infra/flowlogs-bucket.tf
+++ infra/flowlogs-bucket.tf
@@ -1,9 +1,13 @@
 resource "aws_s3_bucket" "tfstate_bucket_umbrella" {
   bucket = "tfstate-bucket-umbrella-${var.owner}"
-  acl    = "private"
 
   tags = {
     Name  = "tfstate-bucket-umbrella-${var.owner}"
     owner = var.owner
   }
+}
+
+resource "aws_s3_bucket_acl" "tfstate_bucket_umbrella_acl" {
+  bucket = aws_s3_bucket.tfstate_bucket_umbrella.id
+  acl    = "private"
 }
