ea900e20c7462546b8727092bc45a7640bb4e686
mdalbes <119487388+mdalbes@users.noreply.github.com>
2025-10-17 11:20:26 +0200

    no role flowlogs

--- infra/flowlogs.tf
+++ infra/flowlogs.tf
@@ -8,40 +8,6 @@ resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
 }
 
 
-
-resource "aws_iam_role" "flow_logs_role" {
-  name = "flow-logs-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "logs.${var.region}.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy" "flow_logs_policy" {
-  name = "flow-logs-policy"
-  role = aws_iam_role.flow_logs_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ]
-      Resource = "*"
-    }]
-  })
-}
-
 resource "aws_flow_log" "vpc_flow_log" {
   traffic_type = "ALL"
   log_destination = aws_s3_bucket.tfstate_bucket_umbrella.arn
