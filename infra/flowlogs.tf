817b591d76e7d9e2906fdaa0826543f9e50f3e9e
mdalbes <119487388+mdalbes@users.noreply.github.com>
2025-10-17 10:42:03 +0200

    vpc flowlogs modif

--- infra/flowlogs.tf
+++ infra/flowlogs.tf
@@ -7,14 +7,48 @@ resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
   }
 }
 
+
+
+resource "aws_iam_role" "flow_logs_role" {
+  name = "flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "logs.${var.region}.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "flow_logs_policy" {
+  name = "flow-logs-policy"
+  role = aws_iam_role.flow_logs_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
 resource "aws_flow_log" "vpc_flow_log" {
+  traffic_type = "ALL"
   log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
   log_destination_type = "cloud-watch-logs"
-  iam_role_arn         = "arn:aws:iam::aws:role/aws-service-role/flow-logs.amazonaws.com/AWSServiceRoleForFlowLogs"
   vpc_id               = aws_vpc.umbrella.id
-  traffic_type         = "ALL"
-
-  tags = {
+  iam_role_arn = aws_iam_role.flow_logs_role.arn
+   tags = {
     owner = var.owner
   }
 }
