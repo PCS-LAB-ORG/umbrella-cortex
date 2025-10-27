8c76225a909f29066301c0a72dd1b2282762ab0c
mdalbes <119487388+mdalbes@users.noreply.github.com>
2025-10-27 10:45:18 +0100

    vuleranle go installation

--- instance IMDSv1 attack path/vulnscript.sh
+++ instance IMDSv1 attack path/vulnscript.sh
@@ -11,3 +11,12 @@ sudo usermod -a -G docker ec2-user
 sudo yum install git -y
 #sudo docker pull jenkins/jenkins:2.138
 #sudo docker run --name jenkins_vuln jenkins/jenkins:2.138
+
+
+## Install old go vulnerables packages
+
+wget https://go.dev/dl/go1.19.1.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.19.1.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+source ~/.profile
